a0a47634a00cbe85d68d16abcf04f2f234843063
asanzdiego <asanzdiego@gmail.com>
2013-12-19 13:53:05 +0100

    delete fonts folder

--- js/util.js
+++ js/util.js
@@ -172,7 +172,7 @@ var util_stringToDateMini = function(string) {
  *****************************/
 
 // version
-var util_version = "jue dic 19 13:48:47 CET 2013"
+var util_version = "jue dic 19 13:52:46 CET 2013"
 
 // Server URL
 var util_server_url = "http://localhost:5000";
