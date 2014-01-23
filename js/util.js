d22a5df0e323eae748116672adeb13920dbc1bd3
asanzdiego <asanzdiego@gmail.com>
2014-01-23 15:14:40 +0100

    change environment.js

--- js/util.js
+++ js/util.js
@@ -175,7 +175,7 @@ var util_stringToDateMini = function(string) {
 var util_version = "jue ene 23 14:49:32 CET 2014"
 
 // Server URL
-var util_server_url = "http://localhost:5000";
+var util_server_url = "http://tweetssentiment.herokuapp.com";
 
 
 /************
--- js/util/environment.js
+++ js/util/environment.js
@@ -7,5 +7,5 @@
 var util_version = "jue ene 23 14:49:32 CET 2014"
 
 // Server URL
-var util_server_url = "http://localhost:5000";
+var util_server_url = "http://tweetssentiment.herokuapp.com";
 
