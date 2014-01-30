edc3f664393a01a6979929a19f1a5a2044a013a3
asanzdiego <asanzdiego@gmail.com>
2014-01-30 21:56:15 +0100

    remove GoogleAnalytics and SmartDataProtection

--- js/util/environment.js
+++ js/util/environment.js
@@ -4,7 +4,7 @@
  *****************************/
 
 // version
-var util_version = "jue ene 23 14:49:32 CET 2014"
+var util_version = "jue ene 30 21:54:54 CET 2014"
 
 // Server URL
 var util_server_url = "http://localhost:5000";
